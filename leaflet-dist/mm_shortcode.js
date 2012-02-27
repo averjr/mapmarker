0f4457838761440b787e3ebad6bc3996873156c8
averjr <andrey.averkiev@solverlabs.com>
2012-02-27 19:20:08 +0200

    some changes

--- leaflet-dist/mm_shortcode.js
+++ leaflet-dist/mm_shortcode.js
@@ -1,36 +1,17 @@
 (function($) {
-    function selectMarkerBox(ed){
-        this.editor = ed;
-        self = this;
-    }
-    selectMarkerBox.prototype = {
-        init : function(){
-            $('.map_marker').live('click', function(){
-                console.log( $(this).text() );
-            })
-
-            $('#insertMarkerSC').live('click', function(e){
-                e.preventDefault();
-                self.insert();
-            })
-        },        
-        setMarkerID : function(id) {
-            this.markerID = id;
-        },
-        
-        insert : function() {
-            this.editor.selection.setContent('[mapsmarker marker="'+ this.markerID +'"]');
-            tb_remove();
-        }
-    }
-
-    
     tinymce.create('tinymce.plugins.mm_shortcode', {
         init : function(ed, url) {
 			ed.addCommand('mm_shortcode', function() {
-                                tb_show('', 'admin-ajax.php?action=get_my_form');
-                                var markerBox = new selectMarkerBox(ed);
-                                markerBox.init();
+                                ed.windowManager.open({
+                                        title : 'Insert/Edit link',
+					file : '/wp-admin/admin-ajax.php?action=get_my_form',
+					width : 450 + parseInt(ed.getLang('example.delta_width', 0)),
+					height : 450 + parseInt(ed.getLang('example.delta_height', 0)),
+                                        inline: 1
+				})
+                                //tb_show('Insert/Edit link', 'admin-ajax.php?action=get_my_form&TB_iframe=true&width=450&height=450');
+                                //var markerBox = new selectMarkerBox(ed);
+                                //markerBox.init();
 			});
                         
 			ed.addButton('mm_shortcode', {title : 'Add maps marker shortcode', cmd : 'mm_shortcode', image: url+'/../img/icon-menu-page.png' });
--- tinyMCE_plugin.php
+++ tinyMCE_plugin.php
@@ -2,6 +2,11 @@
 /**
 Hook into WordPress
 */
+add_action('admin_head', 'marker_select_box_css');
+
+function marker_select_box_css() {
+    echo "<link type='text/css' rel='stylesheet' href='" .  LEAFLET_PLUGIN_URL . 'leaflet-dist/marker_select_box.css' . "' />";
+}
 
 add_action('init', 'mm_shortcode_button');
 
@@ -41,13 +46,167 @@ function add_plugin( $plugin_array ) {
 add_action('wp_ajax_get_my_form',  'get_my_form');
 
 function get_my_form(){
+    global $wpdb;
+
+    $table_name_markers = $wpdb->prefix.'leafletmapsmarker_markers';
+    $table_name_layers = $wpdb->prefix.'leafletmapsmarker_layers';
+    
+    $marklist = $wpdb->get_results("
+            (SELECT l.id, l.name as 'name', l.createdon, 'layer' as 'type' FROM $table_name_layers as l WHERE l.id != '0')
+            UNION
+            (SELECT m.id, m.markername as 'name', m.createdon, 'marker' as 'type' FROM $table_name_markers as m WHERE  m.id != '0')
+            order by createdon DESC", ARRAY_A);
 ?>
 
-<div class="map_marker">1</div>
-<div class="map_marker">2</div>
-<div class="map_marker">3</div>
-<a href="#" id="insertMarkerSC">Create Shortcode</a>                    
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Insert/Edit link</title>
+        
+        <script type='text/javascript' src='/wp-admin/load-scripts.php?c=1&load=jquery'></script>
+        <script type='text/javascript' src='/wp-includes/js/tinymce/tiny_mce_popup.js'></script>
+        <style>
+            #msb_insertMarkerSC{
+                float: right;
+            }
+            #msb_header_description{
+                color:#666666;
+
+            }
+            #msb_serchContainer{
+                padding-left: 40px;
+                margin: 5px 0;
+            }
+            #msb_listContainer{
+                height: 350px;
+                width:100%;
+                overflow-y: scroll;
+
+
+            }
+            #msb_listContainer div{
+                border: 1px solid #ccc;
+            }
+            input.button-primary, button.button-primary, a.button-primary {
+                background: url("/wp-admin/images/button-grad.png") repeat-x scroll left top #21759B;
+                border-color: #298CBA;
+                color: #FFFFFF;
+                font-weight: bold;
+                text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.3);
+                   -moz-box-sizing: content-box;
+                border-radius: 11px 11px 11px 11px;
+                border-style: solid;
+                border-width: 1px;
+                cursor: pointer;
+                font-size: 12px !important;
+                line-height: 13px;
+                padding: 3px 8px;
+                text-decoration: none;
+            }
+            #msb_listContainer .list_item{
+                border-top: none;
+                
+            }
+        </style>
+</head>
+<body>
+
+<span id="msb_header_description">Please select the maps you would like to include</span>
+<div id="msb_serchContainer">Search <input type="text" /></div>
+<div id="msb_listContainer">
+    <div id="msb_listHint" >Please select the maps you would like to include</div>
+    <?php foreach($marklist as $one):?>
+    <div class="list_item">
+        <span><?php echo $one['name']?></span><span><?php echo $one['createdon']?></span>
+        <input type="hidden" value="<?php echo $one['type']?>" name="msb_type">
+        <input type="hidden" value="<?php echo $one['id']?>" name="msb_id">
+    </div>
+    <?php endforeach; ?>  
+</div>
+
+<a href="#" id="msb_cancel">Cancel</a>
+<input class="button-primary" type="button" href="#" id="msb_insertMarkerSC" value="Add shortcode" />       
+
+<script type="text/javascript">
+(function($){
+    function selectMarkerBox(){
+        this.markerID = '';
+        this.mapsmarkerType = '';
+        //this.shortCode = '[mapsmarker '+ this.mapsmarkerType +'="'+ this.markerID +'"]';
+        self = this;
+    }
+        
+    selectMarkerBox.prototype = {
+        init : function(){
+            $('.map_marker').live('click', function(){
+                e.preventDefault();
+                console.log( $(this).text() );
+            })
+
+            $('#msb_insertMarkerSC').live('click', function(e){
+                e.preventDefault();
+                self.insert();
+                self.close();
+            })
+            
+            $('#msb_cancel').live('click', function(e){
+                e.preventDefault();
+                self.close();
+            })
+            
+            $('.list_item').live('click', function(e){
+                e.preventDefault();
+                
+                var id = $(this).find('input[name="msb_id"]').val();
+                var type = $(this).find('input[name="msb_type"]').val(); 
+                
+                self.setMarkerID(id)
+                self.setMarkerType(type);
+
+            })
+        },        
+        setMarkerID : function(id) {
+            this.markerID = id;
+        },
+        setMarkerType : function(type) {
+            switch (type)
+            {
+                case 'layer': 
+                    this.mapsmarkerType = 'layer';
+                    break;
+                case 'marker': 
+                    this.mapsmarkerType = 'marker';
+                    break;
+            }
+        },
+        getShortCode : function(){
+          return '[mapsmarker '+ this.mapsmarkerType +'="'+ this.markerID +'"]';  
+        },
+        insert : function() {
+            tinyMCEPopup.editor.execCommand('mceInsertContent', false, this.getShortCode());
+        },
+        
+        insertMarker : function() {
+            return;
+        },
+        
+        insertList : function() {
+            return;
+        },
+        close : function() {
+            tinyMCEPopup.close();        
+        }
+        
+    }
+    
+    markerBox = new selectMarkerBox();
+    markerBox.init();
+})(jQuery)
+</script>
+
+</body>
+</html>
 <?php    
   exit;
 }
